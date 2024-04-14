a2c74a2d4e5b5ecc039883025bd7012b41fcf00c
lapiedranumerodos <maria.orgniani@estudiante.imagecampus.edu.ar>
2024-04-14 20:02:09 -0300

    adding ice boy script

--- Assets/Scripts/IceBoy.cs
+++ Assets/Scripts/IceBoy.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public class IceBoy : MonoBehaviour
 {
+    [SerializeField] private Material waterMaterial;
+    [SerializeField] private Material iceMaterial;
+    [SerializeField] private GameObject tail;
+
+    [SerializeField] private float waitToFreeze;
+
+    private Collider coll;
+    private Renderer render;
+
+    private void Awake()
+    {
+        render = GetComponent<Renderer>();
+        coll = GetComponent<Collider>();
+        coll.enabled = false;
+
+        if (!render)
+        {
+            Debug.LogError("Renderer component not found on the GameObject.");
+        }
+
+        else render.material = waterMaterial;
+    }
+
+
+    [ContextMenu("Iced!")]
+    private void FreezeWater()
+    {
+        StartCoroutine(WaitToFreeze());
+    }
+
+    private IEnumerator WaitToFreeze()
+    {
+        tail.SetActive(true);
+
+        yield return new WaitForSeconds(waitToFreeze);
+
+        coll.enabled = true;
+        render.material = iceMaterial;
+    }
 
 }
